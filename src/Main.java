2b989238c4c7288c499d7d139361a10aa147a8dd
Zubinator63rus <titanq1991@gmail.com>
2025-03-06 18:00:50 +0400

    test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person("Alex");
-        Person person1 = new Person(12);
+        Person person = new Person("Andrei");
+        Person person1 = new Person(13);
 
 
     }
